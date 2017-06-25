5d33debed626abcd90035ea644c53b28af1ef8e1
peterazp <peter.aspus@gmail.com>
2017-06-25 22:37:09 +0200

    Update HelloWorldMaven.java
    Commit

--- src/main/java/com/oreilly/jenkins/HelloWorldMaven.java
+++ src/main/java/com/oreilly/jenkins/HelloWorldMaven.java
@@ -3,6 +3,6 @@ package com.oreilly.jenkins;
 public class HelloWorldMaven {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from a Maven project.");
+		System.out.println("Hello from a Peter project.");
 	}
 }
